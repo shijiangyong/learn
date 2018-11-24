e66c0f5537c490adce66f5aca5d4064ead6fb7fd
shijy <1224973308@qq.com>
2018-11-24 16:47:48 +0800

    提交信息

--- src/main/java/com/example/mySpringCloud/MySpringCloudApplication.java
+++ src/main/java/com/example/mySpringCloud/MySpringCloudApplication.java
@@ -2,11 +2,17 @@ package com.example.mySpringCloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
 
+/**
+ * @author shijy
+ */
 @SpringBootApplication
+@EnableBinding(Sink.class)
 public class MySpringCloudApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MySpringCloudApplication.class, args);
+        System.out.println("加载springCloud......");
 	}
-
 }
